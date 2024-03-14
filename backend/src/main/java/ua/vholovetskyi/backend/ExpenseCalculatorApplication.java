c80dd4e9ee3e7a385e3bfbd8e4414dfe68115824
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-03-14 17:19:39 +0100

    Init Spring Boot project

---
+++ backend/src/main/java/ua/vholovetskyi/backend/ExpenseCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseCalculatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ua/vholovetskyi/backend/ExpenseCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
